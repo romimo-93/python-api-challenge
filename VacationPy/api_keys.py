312b7d19bb796720719b4d2f30224d632d7b89fa
Romimo <73146401+romimo-93@users.noreply.github.com>
2021-01-06 20:43:52 -0800

    Update api_keys.py

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "b1b265d611d977817377d1070746bd11"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDC1yfVTdKNgDeRkM0f29Z6Md5WMvv0zJg"
+g_key = ""
